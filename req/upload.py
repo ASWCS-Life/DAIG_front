610c644e109aa2ca1dd560b4220f930f36e97ff8
Jin <ekfaktldk11@ajou.ac.kr>
2021-04-29 21:00:32 +0900

    modify & add some folders

--- req/upload.py
+++ req/upload.py
@@ -1,10 +1,8 @@
-import requests
-
 import numpy as np
 
-from tensorflow.keras import layers, models, datasets
+import rest
 
-from auth import get_auth_header
+from tensorflow.keras import layers, models, datasets
 
 # 데이터 준비
 (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
@@ -14,89 +12,50 @@ test_images = test_images.reshape((10000, 28, 28, 1))
 
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
-# 데이터 분할
+# 모델
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
 
-def data_division(train_images, train_lables, num):
-    model_list = []
-    train_img_list = []
-    train_lbl_list = []
+
+# np파일명 설정
+def set_file_name(info, num):
+    file_name = info
+    if (len(str(num)) == 1):
+        file_name += ('00' + str(num))
+    elif (len(str(num)) == 2):
+        file_name += ('0' + str(num))
+    else:
+        file_name += str(num)
+    return file_name + '.npy'
+
+# 데이터 분할
+def data_division(train_images, train_lables, num=100):
     for i in range(num):
-        img_div_pos = (len(train_images)//num)*(i)
-        img_div_end = (len(train_images)//num)*(i+1)
-        lbl_div_pos = (len(train_lables) // num) * (i)
+        img_div_pos = (len(train_images) // num) * i
+        img_div_end = (len(train_images) // num) * (i + 1)
+        lbl_div_pos = (len(train_lables) // num) * i
         lbl_div_end = (len(train_lables) // num) * (i + 1)
 
-        train_img_list.append(train_images[img_div_pos:img_div_end])
-        train_lbl_list.append(train_labels[lbl_div_pos:lbl_div_end])
+        img_file_name = set_file_name('img', i + 1)
+        lbl_file_name = set_file_name('lbl', i + 1)
+        np.save('./np_data/train_img/train_' + img_file_name, train_images[img_div_pos:img_div_end])
+        np.save('./np_data/train_lbl/train_' + lbl_file_name, train_lables[lbl_div_pos:lbl_div_end])
+    return
 
-        model_list.append(models.Sequential())
-        model_list[i].add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-        model_list[i].add(layers.MaxPooling2D((2, 2)))
-        model_list[i].add(layers.Conv2D(64, (3, 3), activation='relu'))
-        model_list[i].add(layers.MaxPooling2D((2, 2)))
-        model_list[i].add(layers.Conv2D(64, (3, 3), activation='relu'))
-        model_list[i].add(layers.Flatten())
-        model_list[i].add(layers.Dense(64, activation='relu'))
-        model_list[i].add(layers.Dense(10, activation='softmax'))
+# 분할 예시
+# data_division(train_images, train_labels, 10000)
 
-    return train_img_list, train_lbl_list, model_list
-
-TIL, TLL, ML = data_division(train_images, train_labels, 5)
-
-# np.array 로 변환
-Til_np = np.array(TIL)
-Tll_np = np.array(TLL)
-Tst_img_np = np.array(test_images)
-Tst_lbl_np = np.array(test_labels)
-
-
-base_url = 'http://localhost:3000'
-
-# 예시로 한명만
+'''
+모델 업로드 예시
 individual_data = {
-    "data":ML[1].to_json()
+    "data":model.to_json()
 }
-
-
-# 모델 데이터 업로드
-def upload(path, data=None):
-    res = requests.post(f'{base_url}/{path}', json=data, headers=get_auth_header())
-
-    #if res.status_code not in [200, 201, 204]:
-    #raise exc.ResponseException(res)
-    print(data)
-    return res.json()
-
-# 학습 시작 요청
-def start(path, params=None):
-    res = requests.get(f'{base_url}/{path}', params=params, headers=get_auth_header())
-
-    #if res.status_code not in [200, 201, 204]:
-    #raise exc.ResponseException(res)
-    return res.json()
-
-
-def result(path, params=None):
-    res = requests.get(f'{base_url}/{path}', params=params, headers=get_auth_header())
-
-    # if res.status_code not in [200, 201, 204]:
-    # raise exc.ResponseException(res)
-    return res.json()
-
-
-def stop(path, params, data=None):
-    res = requests.put(f'{base_url}/{path}', params=params, json=data, headers=get_auth_header())
-
-    # if res.status_code not in [200, 201, 204]:
-    # raise exc.ResponseException(res)
-    return res.json()
-
-
-def status(path, params):
-    res = requests.get(f'{base_url}/{path}', params=params, headers=get_auth_header())
-
-    # if res.status_code not in [200, 201, 204]:
-    # raise exc.ResponseException(res)
-    return res.json()
-
-upload('users',individual_data)
+rest.model_upload('users', individual_data)
+'''
