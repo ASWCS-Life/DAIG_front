a1f2a6f7f77b650e63a06d4e59de4412beb992a9
netroid314 <dyddnrdl3@naver.com>
2021-05-29 13:12:20 +0900

    remove unnecessary

--- ui/test.py
+++ ui/test.py
@@ -38,6 +38,7 @@ def start_learning_internal():
 
 # 학습 모델 받아오기 (현재 더미 데이터 추후 S3)
 def get_model():
+    '''
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]),
         tf.keras.layers.Activation('relu'),
@@ -60,6 +61,42 @@ def get_model():
         tf.keras.layers.Dense(10),
         tf.keras.layers.Activation('softmax')
     ])
+    '''
+    
+    model = tf.keras.models.Sequential()
+
+    weight_decay = 1e-4
+
+    model.add(tf.keras.layers.Conv2D(32, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay), input_shape=x_train.shape[1:]))
+    model.add(tf.keras.layers.Activation('elu'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Conv2D(32, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+    model.add(tf.keras.layers.Activation('elu'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    model.add(tf.keras.layers.Conv2D(64, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+    model.add(tf.keras.layers.Activation('elu'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Conv2D(64, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+    model.add(tf.keras.layers.Activation('elu'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Dropout(0.3))
+
+    model.add(tf.keras.layers.Conv2D(128, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+    model.add(tf.keras.layers.Activation('elu'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Conv2D(128, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+    model.add(tf.keras.layers.Activation('elu'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Dropout(0.4))
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
+  
 
     opt = tf.keras.optimizers.RMSprop(lr=0.0001, decay=1e-6)
     model.compile(loss='categorical_crossentropy',
@@ -80,8 +117,6 @@ def get_train_data():
     return (x_train, y_train), (x_test, y_test)
 
 
-(x_train, y_train), (x_test, y_test) = get_train_data()
-
 if __name__ == '__main__':
     #model = tf.keras.models.load_model('model.h5')
     #result = np.load("result_70.npy",allow_pickle=True)
@@ -89,12 +124,18 @@ if __name__ == '__main__':
     #x_train = np.asarray(np.load('train_data.npy',allow_pickle=True)).astype(np.float32)
     #y_train = np.asarray(np.load('train_label.npy',allow_pickle=True)).astype(np.float32)
 
-    task_num = 50
+    (x_train, y_train), (x_test, y_test) = get_train_data()
 
-    data_split=np.split(x_train,task_num)
-    label_split=np.split(y_train,task_num)
+    mean = np.mean(x_train,axis=(0,1,2,3))
+    std = np.std(x_train,axis=(0,1,2,3))
+    x_train = (x_train-mean)/(std+1e-7)
+    x_test = (x_test-mean)/(std+1e-7)
 
     model = get_model()
+
+    np.save('new_train_data.npy',x_train)
+    np.save('new_train_label.npy',x_test)
+    model.save('new_model.h5')
 
     k = 5
 
@@ -103,15 +144,17 @@ if __name__ == '__main__':
     for i in range(0,k):
         weight_list.append(model.get_weights())
 
-    tmp_weight_1 = model.get_weights()
-    tmp_weight_2 = model.get_weights()
-    tmp_weight_3 = model.get_weights()
-    tmp_weight_4 = model.get_weights()
-    tmp_weight_5 = model.get_weights()
+    epoch = 50
 
-    epoch = 100
+    '''
+    current_time = time.time()
 
-    for index in range(0,10):
+    task_num = 25
+
+    data_split=np.split(x_train,task_num)
+    label_split=np.split(y_train,task_num)
+
+    for index in range(0,5):
         print('step is...',index)
         if(index == 0):
             init_weight = model.get_weights()
@@ -129,9 +172,11 @@ if __name__ == '__main__':
 
         for i in range(0,k):
             model.set_weights(init_weight)
-            model.fit(data_split[index*5+i], label_split[index*5+i], batch_size=32, validation_split=0.2, epochs=epoch, callbacks=[callback], verbose=0)
+            model.fit(data_split[index*5+i], label_split[index*5+i], batch_size=8, validation_split=0.2, epochs=epoch, callbacks=[callback], verbose=0)
             weight_list[i] = model.get_weights()
 
+        print("spent:",time.time()-current_time)
+        current_time = time.time()
         
 
     final_weight = np.array(weight_list[0],dtype = object)
@@ -142,6 +187,10 @@ if __name__ == '__main__':
     final_weight = final_weight.tolist()
 
     model.set_weights(final_weight)
+    '''
+    #model.fit(x_train, y_train, batch_size=8, validation_split=0.2, epochs=epoch, callbacks=[callback], verbose=1)
+            
+
     model.evaluate(x_test, y_test)
     #model.summary()
 
