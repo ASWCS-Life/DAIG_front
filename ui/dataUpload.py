0505d3ea7a7eb07f9de17aea162e68e214552c8c
dyddnrdl3@naver.com <dyddnrdl3@naver.com>
2021-06-15 02:45:07 +0900

    minor bug

--- ui/dataUpload.py
+++ ui/dataUpload.py
@@ -291,7 +291,7 @@ class DataUploadWidget(QWidget):
       return "batch size는 양의 정수형으로 입력해주세요"
     if(not (self.cho_contributor.text().isdecimal() and self.isInt(self.cho_contributor.text()))):
       return "참여자 수는 양의 정수형으로 입력해주세요"
-    if(not (self.cho_valid.text().isdecimal() and (float(self.cho_valid.text()) <= 1 and float(self.cho_valid.text()) > 0))):
+    if(not (float(self.cho_valid.text()) <= 1 and float(self.cho_valid.text()) > 0)):
       return "검증 비율은 0에서 1사이의 실수여야 합니다."
     if(not (self.cho_task.text().isdecimal() and (int(self.cho_task.text()) > 10 and int(self.cho_task.text()) < 100))):
       return "task의 숫자가 너무 크거나 작습니다."
