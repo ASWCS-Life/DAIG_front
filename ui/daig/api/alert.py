bc619fd49ef9f68edde5efa405e83b3b38fef453
ekfaktldk11 <ekfaktldk11@naver.com>
2021-06-14 23:17:30 +0900

    trivial

--- ui/daig/api/alert.py
+++ ui/daig/api/alert.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QMessageBox
 import requests
 import ctypes
 
-def response_alert(self, code):
+def response_alert(code):
     if code not in [200, 201, 270, 204] :
         ctypes.windll.user32.MessageBoxW(0, "ALERT", "API response Error!\nProgram terminating ...", 1)
         raise SystemExit(requests.exceptions.HTTPError)
