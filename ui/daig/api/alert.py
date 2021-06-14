16a6809cd05a249b1b93cc3a922dcd2807b666f8
dyddnrdl3@naver.com <dyddnrdl3@naver.com>
2021-06-15 02:01:12 +0900

    alert box fix

--- ui/daig/api/alert.py
+++ ui/daig/api/alert.py
@@ -4,6 +4,6 @@ import ctypes
 
 def response_alert(code):
     if code not in [200, 201, 270, 204] :
-        ctypes.windll.user32.MessageBoxW(0, "ALERT", "API response Error!\nProgram terminating ...", 1)
+        ctypes.windll.user32.MessageBoxW(0, "API response Error!\nProgram terminating ...", "ALERT", 1)
         raise SystemExit(requests.exceptions.HTTPError)
     pass
