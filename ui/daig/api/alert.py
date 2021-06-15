5350895f11fdb15fa68f33fb156a67a978f1e7f7
dyddnrdl3@naver.com <dyddnrdl3@naver.com>
2021-06-15 12:51:49 +0900

    change alert

--- ui/daig/api/alert.py
+++ ui/daig/api/alert.py
@@ -3,7 +3,10 @@ import requests
 import ctypes
 
 def response_alert(code):
-    if code not in [200, 201, 270, 204] :
+    if code not in [200, 201, 204] :
         ctypes.windll.user32.MessageBoxW(0, "API response Error!\nProgram terminating ...", "ALERT", 0)
         raise SystemExit(requests.exceptions.HTTPError)
+    elif code in [270] : 
+        ctypes.windll.user32.MessageBoxW(0, "Project not finished!", "ALERT", 0)
+        
     pass
